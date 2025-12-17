aa83f287b98392d35d3c13df7af3a61c116f7120
ThaCharly <carlos.gr062008@outlook.com>
2025-12-17 01:28:41 -0300

    Limpio carpetas ignoradas

--- app/build/generated/ap_generated_sources/release/out/com/charly/wallpapermap/BR.java
+++
@@ -1,5 +0,0 @@
-package com.charly.wallpapermap;
-
-public class BR {
-  public static final int _all = 0;
-}
--- app/build/generated/data_binding_trigger/release/com/charly/wallpapermap/DataBindingTriggerClass.java
+++
@@ -1,4 +0,0 @@
-package com.charly.wallpapermap;
-
-@androidx.databinding.BindingBuildInfo
-public class DataBindingTriggerClass {}
